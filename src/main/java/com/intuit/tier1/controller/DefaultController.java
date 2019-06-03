641792249fc11969d334685d1c30f5eee7bd9a54
hlu2 <Hao_Lu@intuit.com>
2019-06-03 09:54:41 -0700

    rest controller

--- src/main/java/com/intuit/tier1/controller/DefaultController.java
+++ src/main/java/com/intuit/tier1/controller/DefaultController.java
@@ -1,9 +1,12 @@
 package com.intuit.tier1.controller;
 
 import com.intuit.tier1.service.MonkeyLearnService;
+import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpEntity;
+import org.springframework.web.bind.annotation.*;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RestController
 public class DefaultController {
@@ -15,8 +18,14 @@ public class DefaultController {
          return "Hello World";
     }
 
-    @RequestMapping("/monkeylearn")
-    public String sample() {
-        return service.sampleRun();
+    @RequestMapping(value="/monkeylearn", method=RequestMethod.GET)
+    public String sample(@RequestParam String body) {
+        return service.sampleRun(body);
+    }
+
+    @RequestMapping(value="/monkeylearn", method=RequestMethod.POST, consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
+    public String postSample(HttpEntity<String> httpEntity) {
+        String json = httpEntity.getBody();
+        return service.sampleRun(json);
     }
 }
--- src/main/java/com/intuit/tier1/service/MonkeyLearnService.java
+++ src/main/java/com/intuit/tier1/service/MonkeyLearnService.java
@@ -8,11 +8,12 @@ import org.springframework.stereotype.Service;
 @Service
 public class MonkeyLearnService {
 
-    public String sampleRun(){
+    public String sampleRun(String s){
         try {
             MonkeyLearn ml = new MonkeyLearn("139af24c72f5a347a3103b8c13fa9355a561828c");
             String modelId = "cl_5WyB9yiD";
-            String[] data = {"Our office needs help, we tried online but did not get any real answers.\n\nWe are updating our system for commission calculations and the company building the application stated that ...So in order for me to connect your QuickBooks environment to the application we are building you, QuickBooks needs a EULA and Data Policy from your team for legal reasons. Without those policy‚how do we get him the authorization to keep going with this project with quickbooks?"};
+            String[] data = new String[1];
+            data[0] = s;
             MonkeyLearnResponse res = ml.classifiers.classify(modelId, data, true);
             return res.arrayResult.toJSONString();
         }catch (MonkeyLearnException e){
